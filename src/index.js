c539cc4c98b8d6cdf3e032db82db6f5b21c5639d
VikashPR <vikashrajendran2806@gmail.com>
2022-05-18 16:36:50 +0530

    Feat: action update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
-function showAlert() {alert("Alert from JS file");}function updateHeading() {document.getElementById('heading').innerHTML = 'Heading changed with JS';}
-function showAlert() {alert("Alert from JS file");}function updateHeading() {document.getElementById('heading').innerHTML = 'Heading changed with JS';}
+function showAlert() {alert("Alert from JS file");}function updateHeading() {document.getElementById('heading').innerHTML = 'Heading changed with JS';}function showAlert() {alert("Alert from JS file");}function updateHeading() {document.getElementById('heading').innerHTML = 'Heading changed with JS';}
